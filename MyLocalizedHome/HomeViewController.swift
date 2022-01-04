ea77ca534bbeeb059505dca223571544e17c6885
julien-guillan <guillan.julien@live.com>
2022-01-04 16:55:25 +0100

    Relay switch

--- MyLocalizedHome/HomeViewController.swift
+++ MyLocalizedHome/HomeViewController.swift
@@ -12,18 +12,28 @@ class HomeViewController: UIViewController {
     
     var homeManager: HMHomeManager!
     var primaryHome: HMHome!
+    var relayPowerStateCharacteristic: HMCharacteristic!
 
     @IBOutlet var houseNameLabel: UILabel!
     @IBOutlet var tempValueLabel: UILabel!
     @IBOutlet var humValueLabel: UILabel!
+    @IBOutlet var relaySwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let manager = HMHomeManager()
         self.homeManager = manager
         manager.delegate = self
+        
     }
     
+    @IBAction func relaySwitchChanged(_ sender: Any) {
+        if self.relaySwitch.isOn {
+            writeValue(characteristic: self.relayPowerStateCharacteristic, value: 1)
+        } else {
+            writeValue(characteristic: self.relayPowerStateCharacteristic, value: 0)
+        }
+    }
     
     func getValueFrom(accessory: HMAccessory, characteristicType: String) -> Any? {
         for service in accessory.services {
@@ -40,7 +50,12 @@ class HomeViewController: UIViewController {
         }
         return nil
     }
-
+    
+    func writeValue(characteristic: HMCharacteristic, value: Int) -> Void {
+        characteristic.writeValue(value, completionHandler: { err in
+            if err != nil { print("ERROR writing value: ", err! ) }
+        })
+    }
 
 }
 
@@ -60,6 +75,23 @@ extension HomeViewController: HMHomeManagerDelegate {
             } else if accessory.name.contains("hum") {
                 let value = self.getValueFrom(accessory: accessory, characteristicType: HMCharacteristicTypeCurrentRelativeHumidity) as! Int
                 self.humValueLabel.text = String(value)
+            } else if accessory.name.lowercased().contains("relais") {
+                for service in accessory.services {
+                    for characteristic in service.characteristics {
+                        //print(characteristic.characteristicType, characteristic.value)
+                        if characteristic.characteristicType == HMCharacteristicTypePowerState {
+                            var value = characteristic.value as! Int
+                            if value == 1 {
+                                value = 0
+                            } else {
+                                value = 1
+                            }
+                            let val = Bool(value == 1)
+                            self.relaySwitch.isOn = val
+                            self.relayPowerStateCharacteristic = characteristic
+                        }
+                    }
+                }
             }
         }
     }
